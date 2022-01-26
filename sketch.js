734ef10cd66f67edc35f501759a9ec10ce8ced98
David <97540571+DTRDJM@users.noreply.github.com>
2022-01-26 17:09:38 +0000

    Update sketch.js
    Added 'function changeDiceValue()' with random and parse functions to generate whole numbers for roll of dice. parseInt lops off the decimal place eg 6.87 becomes 6.
    Added  'if(DiceValue == 1){  DiceValueOne(40, 20, 20);  } to show Dice face one when 1 is randomly generated from the 'Click Here' button. The '==' and '!=' are 2 of the Equality operators within JavaScript Expressions and operators. Notes: Be careful with curly brackets for readability. Be careful of correct Scope for sections of code.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,48 @@
+var DiceValue = 0;
+
 function setup() {
   createCanvas(400, 400);
+  
+  button = createButton('Click Here');
+  button.position(100, 120);
+  button.mousePressed(changeDiceValue);
 }
 
 function draw() {
   background(220);
-  diceValueOne(40, 20, 20);
-
+  
+  if(DiceValue == 1){
+      //result = DiceValueOne;
+  DiceValueOne(40, 20, 20);
+      //window.prompt(DiceValueOne);
+  }
+  if(DiceValue == 2){
+  DiceValueTwo(40, 20, 20);
+  }
 }
-
+  function changeDiceValue() {
+  let val = random(1,7); //parsing here lops off decimals
+  //console.log("changeDiceValue called val = " + val)
+  DiceValue = parseInt(val);
+  console.log("parsed number = " + DiceValue);
+  
+  //let val = random(6);
+  //parseInt(val);
+  //let string = val - NOT REQUIRED see parseInt in Mozilla
+  //let number = parseInt(string);
+  
+  //let val = Math.floor(Math.random() * 6);
+}
 //This is a user defined function for drawing a dice with the value one.
-function diceValueOne(size, xPoint, yPoint){
+
+function DiceValueOne(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
   point((size/2) + xPoint,(size/2) + yPoint);
 }
 
-function diceValueTwo(size, xPoint, yPoint){
+function DiceValueTwo(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
