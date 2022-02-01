ffcc121fe06f59cd2f95f1dc8b58e86efbfcb342
David <97540571+DTRDJM@users.noreply.github.com>
2022-02-01 10:43:56 +0000

    Update sketch.js
    Now randomly selecting one of the six DiceValue Array positions in order to simulate rolling the dice instead of generating 1 to 6 parsed numbers as in DiceSimulator branch. Since it uses an Array index which has to begin at zero, 0 refs 1, 1 refs 2 etc. These are still whole integers so parseInt() function still required.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,42 @@
+var DiceArrayPos = 0;  //Array Index has to begin at zero
+
+var DiceValueFunctionArray = [DiceValueOne, DiceValueTwo, DiceValueThree, DiceValueFour, DiceValueFive, DiceValueSix];
+
 function setup() {
   createCanvas(400, 400);
+  
+  //console.log(DiceValueFunctionArray) Dispalys the six DiceValue Array positions in the variable above
+  
+  button = createButton('Click Here');
+  button.position(100, 120);
+  button.mousePressed(changeDiceArrayPos);
 }
 
 function draw() {
   background(220);
-  diceValueOne(40, 20, 20);
-
+  
+  DiceValueFunctionArray[DiceArrayPos](40,20,20);
+  //if(DiceValue == 1){
+  //DiceValueOne(40, 20, 20);
+  //}
 }
-
+  function changeDiceArrayPos() {
+  //let val = random(0,6); //parsing here lops off decimals
+  DiceArrayPos = parseInt(random(0,6));
+  //Now randomly selecting one of the six DiceValue Array positions in order to simulate rolling the dice. 
+  console.log(DiceArrayPos);
+//}
+}
 //This is a user defined function for drawing a dice with the value one.
-function diceValueOne(size, xPoint, yPoint){
+
+function DiceValueOne(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
   point((size/2) + xPoint,(size/2) + yPoint);
 }
 
-function diceValueTwo(size, xPoint, yPoint){
+function DiceValueTwo(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
@@ -24,7 +44,7 @@ function diceValueTwo(size, xPoint, yPoint){
   point((size * 0.75) + xPoint,(size * 0.75) + yPoint);
 }
 
-function diceValueThree(size, xPoint, yPoint){
+function DiceValueThree(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
@@ -33,7 +53,7 @@ function diceValueThree(size, xPoint, yPoint){
   point((size * 0.75) + xPoint,(size * 0.75) + yPoint);
 }
 
-function diceValueFour(size, xPoint, yPoint){
+function DiceValueFour(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
@@ -43,7 +63,7 @@ function diceValueFour(size, xPoint, yPoint){
   point((size * 0.75) + xPoint,(size * 0.75) + yPoint);
 }
 
-function diceValueFive(size, xPoint, yPoint){
+function DiceValueFive(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
@@ -54,7 +74,7 @@ function diceValueFive(size, xPoint, yPoint){
   point((size/2) + xPoint,(size/2) + yPoint);
 }
 
-function diceValueSix(size, xPoint, yPoint){
+function DiceValueSix(size, xPoint, yPoint){
   strokeWeight(size * 0.01);
   square(xPoint, yPoint, size);
   strokeWeight(size * 0.15);
